6c61d2856e5a7fe68da6682b198fcbd8deaf5fad
Diya Salam <sdiya.salam.github@gmail.com>
2025-11-06 15:32:15 -0500

    add

--- src/main/java/com/udemy/rest/webservices/restful_web_services/user/UserResource.java
+++ src/main/java/com/udemy/rest/webservices/restful_web_services/user/UserResource.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,4 +47,8 @@ public class UserResource {
 		return ResponseEntity.created(location).build();
 	}
 	
+	@DeleteMapping("/delete/{id}")
+	public void deleteUser(@PathVariable int id) {
+		userDaoService.deleteById(id);
+	}
 }
