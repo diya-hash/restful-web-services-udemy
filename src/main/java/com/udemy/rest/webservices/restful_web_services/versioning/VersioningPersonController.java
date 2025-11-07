e4e705970e6e02fbb98d4c065dee08826013159a
Diya Salam <sdiya.salam.github@gmail.com>
2025-11-07 11:20:20 -0500

    add

---
+++ src/main/java/com/udemy/rest/webservices/restful_web_services/versioning/VersioningPersonController.java
@@ -0,0 +1,33 @@
+package com.udemy.rest.webservices.restful_web_services.versioning;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VersioningPersonController {
+
+	@GetMapping("/v1/person")
+	public PersonV1 getFirstVersionPerson() {
+		return new PersonV1("Bob Charlie");
+	}
+	@GetMapping("/v2/person")
+	public PersonV2 getSecondVersionPerson() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
+	@GetMapping(path = "/person", params = "version=1")
+	public PersonV1 getFirstVersionPersonRequestParameter() {
+		return new PersonV1("Bob Charlie");
+	}
+	@GetMapping(path = "/person", params = "version=2")
+	public PersonV2 getSecondVersionPersonRequestParameter() {
+		return new PersonV2(new Name("Bob ", "Charlie"));
+	}
+	@GetMapping(path = "/person/header", headers = "X-API-VERSION=1")
+	public PersonV1 getFirstVersionPersonRequestHeader() {
+		return new PersonV1("Bob Charlie");
+	}
+	@GetMapping(path = "/person/header", headers = "X-API-VERSION=2")
+	public PersonV2 getSecondVersionPersonRequestHeader() {
+		return new PersonV2(new Name("Bob ", "Charlie"));
+	}
+}
