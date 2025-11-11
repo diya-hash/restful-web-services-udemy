f43952a670e6dba4efa014eadf6db30aefc6754b
Diya Salam <sdiya.salam.github@gmail.com>
2025-11-11 10:40:37 -0500

    added dynamic filtering

--- src/main/java/com/udemy/rest/webservices/restful_web_services/filtering/SomeBean.java
+++ src/main/java/com/udemy/rest/webservices/restful_web_services/filtering/SomeBean.java
@@ -1,12 +1,15 @@
 package com.udemy.rest.webservices.restful_web_services.filtering;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonFilter;
 
-@JsonIgnoreProperties("field1")
+//import com.fasterxml.jackson.annotation.JsonIgnore;
+//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+//@JsonIgnoreProperties("field1")
+@JsonFilter("SomeBeanFilter")
 public class SomeBean {
 	private String field1;
-	@JsonIgnore
+//	@JsonIgnore
 	private String field2;
 	private String field3;
 	
